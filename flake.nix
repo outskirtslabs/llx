4f7b1e53186db43e730c20d0114a72a697734d13
Casey Link <casey@outskirtslabs.com>
2026-03-23 12:04:11 +0100

    pkg with nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,22 @@
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # tracks nixpkgs unstable branch
     devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
     devenv.url = "https://flakehub.com/f/ramblurr/nix-devenv/*";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    clj-nix.url = "github:jlesquembre/clj-nix";
+    clj-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
-    {
+    inputs@{
       self,
+      clj-nix,
       devenv,
       devshell,
       ...
     }:
     devenv.lib.mkFlake ./. {
+      inherit inputs;
 
       treefmtConfig = {
         programs = {
@@ -29,7 +35,70 @@
       withOverlays = [
         devshell.overlays.default
         devenv.overlays.default
+        clj-nix.overlays.default
       ];
+      packages =
+        let
+          mkLlxPackage =
+            pkgs:
+            let
+              root = toString ./.;
+              gitRev =
+                if self ? rev then
+                  self.rev
+                else if self ? dirtyRev then
+                  self.dirtyRev
+                else
+                  "dirty";
+              projectSrc = pkgs.lib.cleanSourceWith {
+                src = ./.;
+                filter =
+                  path: _type:
+                  let
+                    rel = pkgs.lib.removePrefix (root + "/") (toString path);
+                    base = builtins.baseNameOf path;
+                  in
+                  !(
+                    base == ".git"
+                    || rel == "result"
+                    || pkgs.lib.hasPrefix "node_modules/" rel
+                    || pkgs.lib.hasPrefix "target/" rel
+                    || pkgs.lib.hasPrefix ".shadow-cljs/" rel
+                    || pkgs.lib.hasPrefix ".cpcache/" rel
+                    || pkgs.lib.hasPrefix "tmp/" rel
+                  );
+              };
+            in
+            pkgs.mkCljLib {
+              inherit projectSrc;
+              name = "com.outskirtslabs/llx";
+              version = "0.0.1";
+              nativeBuildInputs = [
+                pkgs.coreutils
+                pkgs.nodejs
+              ];
+              GIT_REV = gitRev;
+              JAVA_HOME = pkgs.jdk25.home;
+              buildCommand = ''
+                export JAVA_HOME="${pkgs.jdk25.home}"
+                export JAVA_CMD="${pkgs.jdk25}/bin/java"
+                clojure -M:dev:kaocha :unit
+                clojure -M:dev:shadow-cljs compile kaocha-test
+                node target/kaocha-tests.js
+                clojure -T:build jar
+              '';
+            };
+        in
+        {
+          default = mkLlxPackage;
+        };
+      checks =
+        let
+          mkLlxPackage = pkgs: self.packages.${pkgs.system}.default;
+        in
+        {
+          default = mkLlxPackage;
+        };
       devShell =
         pkgs:
         pkgs.devshell.mkShell {
@@ -40,7 +109,11 @@
           # https://numtide.github.io/devshell
           commands = [
           ];
-
+          packages = [
+            pkgs.deps-lock
+            pkgs.jdk25
+            pkgs.nodejs
+          ];
         };
     };
 }
